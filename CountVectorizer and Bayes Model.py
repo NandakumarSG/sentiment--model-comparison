c7dc515de1eeda92769443cd90a669ad7131d3c3
Nandakumar S G <nandukpz@gmail.com>
2018-06-25 12:19:13 +0530

    Update CountVectorizer and Bayes Model.py

--- CountVectorizer and Bayes Model.py
+++ CountVectorizer and Bayes Model.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import nltk
 from nltk.corpus import stopwords
 %matplotlib inline
@@ -11,10 +9,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, classification_report
 import pickle
-from sklearn.svm import SVC
-from sklearn.feature_extraction.text import TfidfVectorizer
-def text_process(text):
 
+
+def text_process(text):
     '''
     Takes in a string of text, then performs the following:
     1. Remove all punctuation
@@ -28,8 +25,8 @@ yelp = pd.read_csv('yelp.csv')
 yelp_class = yelp[(yelp['stars'] == 1) | (yelp['stars'] == 5)]
 X = yelp_class['text']
 y = yelp_class['stars']
-bow_transformer = CountVectorizer(analyzer=text_process).fit(X)
-X = bow_transformer.transform(X)
+Count_transformer = CountVectorizer(analyzer=text_process).fit(X)
+X = Count_transformer.transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
 nb = MultinomialNB()
 nb.fit(X_train, y_train)
