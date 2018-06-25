41ced075b59aca0d4318227a8af380ce09fcd712
Nandakumar S G <nandukpz@gmail.com>
2018-06-25 11:58:51 +0530

    Different model python scripts

---
+++ TfIdfVectorizer and SVC.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import nltk
+from nltk.corpus import stopwords
+%matplotlib inline
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix, classification_report
+import pickle
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+def text_process(text):
+
+    '''
+    Takes in a string of text, then performs the following:
+    1. Remove all punctuation
+    2. Remove all stopwords
+    3. Return the cleaned text as a list of words
+    '''
+    nopunc = [char for char in text if char not in string.punctuation]
+    nopunc = ''.join(nopunc)
+    return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+yelp = pd.read_csv('yelp.csv')
+yelp_class = yelp[(yelp['stars'] == 1) | (yelp['stars'] == 5)]
+X = yelp_class['text']
+y = yelp_class['stars']
+
+bow_transformer = TfidfVectorizer(analyzer=text_process).fit(X)
+X = bow_transformer.transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+nb = SVC(kernel='linear')
+nb.fit(X_train, y_train)
+preds = nb.predict(X_test)
+
+
+print(classification_report(y_test, preds))
